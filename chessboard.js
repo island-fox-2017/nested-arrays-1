91a7cd0d0716d2a5817ed5f93e7d4c21eac607d0
rahid <hidayat.85@gmail.com>
2017-06-21 16:18:45 +0700

    nested array 1 done

--- chessboard.js
+++ chessboard.js
@@ -2,13 +2,42 @@
 
 const makeChessboard = () => {
   let chessboard = []
+  let bidak = ['Benteng', 'Kuda', 'Peluncur', 'Raja', 'Perdana Menteri', 'Peluncur', 'Kuda', 'Benteng'];
 
+  for (let i=0; i<8; i++) {
+    chessboard.push([]);
+    if (i == 0) {
+      for (let j=0; j<bidak.length; j++) {
+        chessboard[i].push(bidak[j]+' Hitam');
+      }
+    }
+    else if (i==1) {
+      for (let j=0; j<8; j++) {
+        chessboard[1].push('Pion Hitam');
+      }
+    }
+    else if (i>=2 && i<=5) {
+      chessboard[i].push(' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+    }
+    else if (i==6) {
+      for (let j=0; j<8; j++) {
+        chessboard[6].push('Pion Putih');
+      }
+    }
+    else if (i==7) {
+      for (let j=0; j<bidak.length; j++) {
+        chessboard[7].push(bidak[j]+' Putih');
+      }
+    }
+  }
   // ... write your code here
   return chessboard
 }
 
-const printBoard = x => {
+const printBoard = chessboard => {
   // ... write your code here
+  for (let i=0; i<8; i++)
+    console.log(chessboard[i].join(' | '));
 }
 
 printBoard(makeChessboard())
--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,34 @@
 function tic_tac_toe_board () {
   // ...
+  board = [];
+  var countX = 0;
+  var countO = 0;
+  for (i=0; i<3; i++) {
+    board.push([]);
+    for (j=0; j<3; j++) {
+      var numRandom = Math.floor(Math.random() * 2) + 1
+      if (numRandom === 1) {
+        if(countX < 5) {
+          board[i].push('X');
+          countX++;
+        }
+        else {
+          board[i].push('O');
+        }
+      }
+      else {
+        if (countO < 5) {
+          board[i].push('O');
+          countO++;
+        }
+        else {
+          board[i].push('X');
+        }
+
+      }
+    }
+  }
+  return board;
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
