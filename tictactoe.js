fb33774f9a4a4facd20a57078ea989bd592fabc0
pdvega <pdvegaa@gmail.com>
2017-06-21 12:00:24 +0700

    tictactoe done

--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,34 @@
 function tic_tac_toe_board () {
   // ...
+
+  let letterX = 'x',
+    letterO = 'o',
+    result = [],
+    xCount = 0,
+    oCount = 0,
+    row,
+    col,
+    arr,
+    randomDice;
+    
+    
+    for(row=0; row<3; row++){
+        arr=[]
+      for(col=0; col<3; col++){
+        randomDice = Math.ceil(Math.random()*3)
+        if(randomDice == 1 && xCount<5){
+          arr.push(letterX)
+          xCount++
+        }else if(oCount<5){
+          arr.push(letterO)
+          oCount++
+        }else{
+          arr.push(letterX)
+        }
+      }
+      result.push(arr)
+    }
+    return result
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
