a93e6205e267bcc28e983ecb18ae3fec53867b3d
akbarsahata <akbarsahata@outlook.co.id>
2017-06-21 09:03:27 +0700

    Template

---
+++ chessboard.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const makeChessboard = () => {
+  let chessboard = []
+
+  // ... write your code here
+  return chessboard
+}
+
+const printBoard = x => {
+  // ... write your code here
+}
+
+printBoard(makeChessboard())
+
+module.exports = {
+  makeChessboard,
+  printBoard
+}
---
+++ test.js
@@ -0,0 +1,20 @@
+const assert = require('assert')
+
+const { makeChessboard, printBoard } = require('./chessboard.js')
+const Spreadsheet = require('./spreadsheet.js')
+
+const chessboard = makeChessboard()
+describe('Chessboard Release 0', () => {
+  it('should "Benteng Putih" if pos is 7, 0', () => {
+    assert.deepEqual(chessboard[7][0], 'Benteng Putih')
+  })
+  it('should "Kuda Hitam" if pos is 0, 1', () => {
+    assert.deepEqual(chessboard[0][1], 'Kuda Hitam')
+  })
+  it('should "Pion Putih" if pos is 6, 4', () => {
+    assert.deepEqual(chessboard[6][4], 'Pion Putih')
+  })
+  it('should "Gajah Hitam" if pos i 0, 5', () => {
+    assert.deepEqual(chessboard[0][5], 'Gajah Hitam')
+  })
+})
