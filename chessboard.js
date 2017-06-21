d1c09249b66b6cb3aa1168b982ae88b350ff7fb5
name": null <email": null>
2017-06-21 20:50:55 +0700

    chessboard & tictactoe DONE

--- chessboard.js
+++ chessboard.js
@@ -1,14 +1,45 @@
 'use strict'
 
 const makeChessboard = () => {
-  let chessboard = []
+  let board = [];
+  let hitam = ["Benteng Hitam", "Kuda Hitam", "Peluncur Hitam", "Raja Hitam", "Ratu Hitam", "Peluncur Hitam", "Kuda Hitam", "Benteng Hitam",];
+  let putih = ["Benteng Putih", "Kuda Putih", "Peluncur Putih", "Raja Putih", "Ratu Putih", "Peluncur Putih", "Kuda Putih", "Benteng Putih",];
+  let hitamPion = ["pion hitam"];
+  let putihPion = ["pion putih"];
 
-  // ... write your code here
-  return chessboard
+  for (let i = 0; i < 8; i++) {
+    if (i === 0)
+      board.push(hitam);
+    else if (i === 7)
+      board.push(putih);
+    else if(i === 1) {
+      for (let j = 0; j < 8; j++) {
+        hitamPion.push("pion hitam");
+      }
+      board.push(hitamPion);
+    }
+    else if(i === 6){
+      for (let j = 0; j < 8; j++) {
+        putihPion.push("pion putih");
+      }
+      board.push(putihPion);
+    }
+    else {
+      let kosong = [];
+      for (let j = 0; j < 8; j++) {
+        kosong.push("z");
+      }
+      board.push(kosong);
+    }
+  }
+  return board;
 }
 
-const printBoard = x => {
+const printBoard = board => {
   // ... write your code here
+  for (let i = 0; i < 8; i++) {
+    console.log(board[i].join(" | "));
+  }
 }
 
 printBoard(makeChessboard())
--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,29 @@
 function tic_tac_toe_board () {
   // ...
+  let arr = [], result = "", okay = 0, okay2 = 0;
+  for (let i = 0; i < 3; i++) {
+    arr.push([]);
+    for (let j = 0; j < 3; j++) {
+      if ( (okay+okay2) < 9) {
+        if (Math.floor(Math.random()*2+1) == 1 && okay < 5) {
+          arr[i][j] = "X";
+          okay++;
+        }
+        else if (okay2 < 5) {
+          arr[i][j] = "O";
+          okay2++;
+        }
+        else arr[i][j] = "X";
+      }
+    }
+  }
+  for (let k = 0; k < 3; k++) {
+    arr[k] = arr[k].join(' | ')
+    for (let l = 0; l < 3; l++) {
+      result = arr.join('\n');
+    }
+  }
+  return result;
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
