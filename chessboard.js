a92c221e8c6935a418ac34c183d5e80619704ee9
adnin31 <adnin.ris31@gmail.com>
2017-06-21 17:10:06 +0700

    update chessboar dan tictactoe

--- chessboard.js
+++ chessboard.js
@@ -4,11 +4,53 @@ const makeChessboard = () => {
   let chessboard = []
 
   // ... write your code here
+  const nameChessWhite = ['Benteng Putih', 'Kuda Putih', 'Peluncur Putih ','Ratu Putih','Raja Putih','Pion Putih' ];
+  const nameChessBlack = ['Benteng Hitam', 'Kuda Hitam', 'Peluncur Hitam ','Raja Hitam','Ratu Hitam','Pion Hitam' ];
+  for (let baris = 0; baris < 8; baris++) {
+    chessboard.push([]);
+    let tambah = 0;
+    for (let kolom = 0; kolom < 8; kolom++) {
+
+        //baris hitam
+        if (baris ==0 && kolom <=4) {
+          chessboard[baris].push(nameChessBlack[kolom])
+        } else if ( baris == 0 && kolom > 4) {
+          chessboard[baris].push(nameChessBlack[tambah]);
+          tambah++
+        }
+
+        //pion
+        else if (baris == 1) {
+          chessboard[baris].push(nameChessBlack[5])
+        }else if (baris==6 ) {
+          chessboard[baris].push(nameChessWhite[5])
+        }
+
+        //bidak putih
+        else if (baris ==7 && kolom <=4) {
+          chessboard[baris].push(nameChessWhite[kolom])
+        } else if ( baris == 7 && kolom > 4) {
+          chessboard[baris].push(nameChessWhite[tambah]);
+          tambah++
+        }
+
+        else {
+          chessboard[baris].push("=")
+        }
+    }
+  }
+
   return chessboard
 }
 
+//console.log(makeChessboard());
+
 const printBoard = x => {
   // ... write your code here
+  for (let baris = 0; baris < x.length; baris++) {
+    console.log(x[baris].join(" | ") );
+  }
+
 }
 
 printBoard(makeChessboard())
--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,25 @@
 function tic_tac_toe_board () {
   // ...
+  const board = []
+  let sizeBoard = 3
+  var x = 0 , o=0
+  for (let i = 0; i < sizeBoard ; i++) {
+    board.push([])
+  }
+  for (let i = 0; i < sizeBoard ; i++) {
+    for (let j = 0; j < sizeBoard; j++) {
+      let dahulu  = Math.floor(Math.random()*2+0)
+      if(dahulu == 1 && x<=5 || o>=5 ){
+        board[j].push('x')
+        x++
+      }else{
+        board[j].push('o')
+        o++
+      }
+    }
+  }
+
+  return board
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
