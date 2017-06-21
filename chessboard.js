1543266cf0b832a53d713723dc9b9c0c3428b145
pdvega <pdvegaa@gmail.com>
2017-06-21 11:39:50 +0700

    chessboard done tictactoe in progress

--- chessboard.js
+++ chessboard.js
@@ -1,17 +1,51 @@
 'use strict'
 
 const makeChessboard = () => {
-  let chessboard = []
-
+  
   // ... write your code here
-  return chessboard
+  let chessMan = ['Benteng', 'Kuda', 'Gajah', 'Ratu', 'Raja', 'Menteri', 'Kuda', 'Benteng'],
+    color = ['Black', 'White'],
+    pion = ['Pion'],
+    chessboard = [],
+    board = [],
+    row,
+    col;
+    
+    for(row=0; row<8; row++){
+      for(col=0; col<8; col++){
+        if(row == 0){
+          chessboard.push(chessMan[col] + ' ' + color[0])
+        }else if(row == 1){
+          chessboard.push(pion[0] + ' ' + color[0])
+        }else if(row == 6){
+          chessboard.push(pion[0] + ' ' + color[1])
+        }else if(row == 7){
+          chessboard.push(chessMan[col] + ' ' + color[1])
+        }else {
+          chessboard.push(' ')
+        }
+      }
+    }
+
+  let i,j, square=8;
+  for(i=0, j=chessboard.length; i<j; i+=square){
+    // console.log(i);
+    // console.log(j);
+    board.push(chessboard.slice(i,i+square))
+    // console.log(chessboard.slice(i,i+square));
+  }
+    return board
 }
 
 const printBoard = x => {
   // ... write your code here
+  for(let k=0; k<8; k++){
+    x[k] = x[k].join(' | ')
+  }
+  return x
 }
 
-printBoard(makeChessboard())
+console.log(printBoard(makeChessboard()))
 
 module.exports = {
   makeChessboard,
