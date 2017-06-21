9600be172bbb54f4cf4a62a3d202e3acc71c59fb
Ahmad Fajar <shin.fajar@gmail.com>
2017-06-21 15:36:19 +0700

    ahmad fajar - tic tac toe

--- chessboard.js
+++ chessboard.js
@@ -1,17 +1,48 @@
 'use strict'
 
 const makeChessboard = () => {
-  let chessboard = []
+  // --- board ---
+  let grid = () => {
+    let line = [];
+    for (var x = 0; x < 8; x++) {
+      line.push([])
+      for (var y = 0; y < 8; y++) {
+        line[x].push('[   ]')
+      }
+    }
+    return line;
+  }
 
-  // ... write your code here
+  let chessboard = grid();
+
+  // --- pieces ---
+  const pieces = [   ['pawn'], ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook']  ];
+  const color = ['black', 'white'];
+
+  // --- placing ---
+  for (var row = 0; row < 8; row++) {
+    for (var col = 0; col < 8; col++) {
+      if (row === 0) chessboard[row][col] = `${color[0]} ${pieces[1][col]}`;
+      if (row === 1) chessboard[row][col] = `${color[0]} ${pieces[0]}`;
+      if (row === 6) chessboard[row][col] = `${color[1]} ${pieces[0]}`;
+      if (row === 7) chessboard[row][col] = `${color[1]} ${pieces[1][col]}`;
+    }
+  }
+
   return chessboard
 }
 
+// console.log(makeChessboard());
+
 const printBoard = x => {
-  // ... write your code here
+  let display = [];
+  for (var i = 0; i <= x.length - 1; i++) display.push(x[i].join(' | '))
+  // console.log(display);
+
+  return console.log(display.join('\n'));
 }
 
-printBoard(makeChessboard())
+printBoard(makeChessboard());
 
 module.exports = {
   makeChessboard,
--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,34 @@
 function tic_tac_toe_board () {
-  // ...
-}
+  let board = [];
+  let nX = 0;
+  let nO = 0;
+  let rand = () => Math.floor(Math.random() * 2);  // 0 - 1
+
+  for (var row = 0; row < 3; row++) {
+    let line = [];
+    for (var col = 0; col < 3; col++) {
+      if (rand() === 0) {
+        if (nO <= 4) {
+          line.push('O');
+          nO++;
+        } else {
+          line.push('X');
+        }
+
+      } else {
+        if (nX <= 4) {
+          line.push('X');
+          nX++;
+        } else {
+          line.push('O')
+        }
+      }
+    }
+    board.push(line);
+  }
+
+  // return rand();
+  return `${board[0].join(' ')}\n${board[1].join(' ')}\n${board[2].join(' ')}`;
+} // --- tic-tac-toe
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
