0e98c4b046bcddab8a8547023b1845fa28593622
rusli abdul gani <rusli.gani88@gmail.com>
2017-06-21 14:10:08 +0700

    edit random mode

--- chessboard.js
+++ chessboard.js
@@ -1,14 +1,49 @@
 'use strict'
 
 const makeChessboard = () => {
-  let chessboard = []
+  var chessboard = []
 
   // ... write your code here
+  let chessPieces = ['Benteng','Kuda','Menteri','Raja','Ratu','Menteri','Kuda','Benteng'];
+  
+  // for (let i = 0 ; i <= 8 ; i++)
+  
+  for (let i = 0 ; i < 8 ; i++){
+    if (i === 0 ){
+      chessboard.push([]);
+      for(let i = 0 ; i < 8 ; i++){
+        chessboard[0].push(chessPieces[i]+' hitam')
+      }
+    } else if (i === 1){
+      chessboard.push([])
+      for(let i = 0 ; i < 8 ; i++){
+        chessboard[1].push('Pion Hitam')
+      }
+    } else if (i === 6){
+      chessboard.push([])
+      for(let i = 0 ; i < 8 ; i++){
+        chessboard[6].push('Pion Putih')
+      }
+    } else if (i === 7){
+      chessboard.push([])
+      for(let i = 0 ; i < 8 ; i++){
+        chessboard[7].push(chessPieces[i] + ' putih')
+      }
+    }else {
+      chessboard.push(['0','0','0','0','0','0','0','0'])
+      }
+    }
+
+
+// console.log(chessboard);
   return chessboard
 }
 
 const printBoard = x => {
   // ... write your code here
+  for (let i = 0 ; i < 8 ; i++){
+    console.log(x[i].join(`|`));
+  }
 }
 
 printBoard(makeChessboard())
--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,45 @@
-function tic_tac_toe_board () {
-  // ...
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  // console.log(array);
+  return array;
 }
 
-console.log(tic_tac_toe_board()) // => make a random tic tic board
+
+let ticBox = [];
+let batas0 = 0;
+
+for (let i = 0; i <= 9; i++) {
+  let batasX = Math.floor(Math.random() * 1) + 4;
+  if (i <= batasX) {
+    ticBox.push('X')
+  } else {
+    ticBox.push('0');
+  }
+}
+
+function tic_tac_toe_board() {
+  // ...
+  var finalTicTacToeBox = [];
+  let j, i, potong = 3
+  var acak = shuffleArray(ticBox);
+  for (i = 0, j = acak.length; i < j; i += potong) {
+    finalTicTacToeBox.push(acak.slice(i, i + potong));
+  }
+  return finalTicTacToeBox;
+}
+
+function printBoardTicTacToe(tictac) {
+  let print = ''
+  for (let i = 0; i < 3; i++) {
+    print += tictac[i].join('|') + '\n';
+  }
+  return print
+}
+
+//console.log(shuffleArray(ticBox));
+console.log(printBoardTicTacToe(tic_tac_toe_board())) // => make a random tic tic board
