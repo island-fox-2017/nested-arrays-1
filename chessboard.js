c02299baf09197e0a558f2d083d89ff3f0623303
Christian <email": null>
2017-06-22 11:15:18 +0700

    nested array 1 selesai

--- chessboard.js
+++ chessboard.js
@@ -1,14 +1,46 @@
 'use strict'
 
 const makeChessboard = () => {
-  let chessboard = []
+  let catur = ['Benteng','Kuda','Peluncur','Perdana Menteri','Raja', 'Peluncur','Kuda','Benteng'];
+  let pion = 'Pion';
+  var chessboard = [];
+  for(var i=0; i<catur.length; i++) {
+    //fungsi ny ap?
+    chessboard.push([]);
+    for(var j=0; j<catur.length; j++){
+    if(i==0) {
+      chessboard[i].push(catur[j]+' Hitam');
+    }
+    else if(i==1) {
+    chessboard[i].push(pion);
+    }
+    else if(i>1 && i<6) {
+      chessboard[i].push('');
+    }
+    else if(i==6) {
+      chessboard[i].push(pion);
+    }
+    else {
+      chessboard[i].push(catur[j]+' Putih');
+    }
+  }
 
-  // ... write your code here
+
+  }
+
+    // ... write your code here
   return chessboard
 }
 
-const printBoard = x => {
-  // ... write your code here
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+const printBoard = chessboard => {
+
+  for (var i = 0; i < 8; i++) {
+  console.log(chessboard[i].join(' | '));  // ... write your code here
+}
+
+
 }
 
 printBoard(makeChessboard())
--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,30 @@
 function tic_tac_toe_board () {
-  // ...
+  var x = 0;
+  var o = 0;
+  var toeBoard = [];
+
+  for (var i = 0; i <= 2; i++) {
+    var tabelBaru = [];
+
+    for(var j=0; j<=2; j++) {
+    var random = Math.floor(Math.random()*2);
+    if(random == 1 && x <5) {
+
+      tabelBaru.push('x');
+      x += 1
+    }
+    else if(o<=4){
+
+      tabelBaru.push('o');
+      o+=1
+    }
+  }toeBoard.push(tabelBaru);
+
+}return toeBoard;
 }
+
+     // ...
+
+
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
