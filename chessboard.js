07c3b045ed0ef6b4d019043123afe82f03dbaed9
biliman <fickry.bil.iman@gmail.com>
2017-06-23 23:52:37 +0700

    nested array 1 done

--- chessboard.js
+++ chessboard.js
@@ -1,14 +1,58 @@
 'use strict'
 
 const makeChessboard = () => {
-  let chessboard = []
-
   // ... write your code here
-  return chessboard
+  let baris8 = [];
+  let baris1 = [];
+  let baris7 = [];
+  let baris2 = [];
+  let baris3456 = [ , , , , , , , , ];
+  let result = [];
+
+  for (let i = 0; i < 8; i++) {
+    if (i === 0 || i === 7) {
+      baris8[i] = 'Benteng Hitam';
+      baris1[i] = 'Benteng Putih';
+    } else if (i === 1 || i === 6) {
+      baris8[i] = 'Kuda Hitam';
+      baris1[i] = 'Kuda Putih';
+    } else if (i === 2 || i === 5) {
+      baris8[i] = 'Peluncur Hitam';
+      baris1[i] = 'Peluncur Putih';
+    } else if (i === 3) {
+      baris8[i] = 'Ratu Hitam';
+      baris1[i] = 'Ratu Putih';
+    } else if (i === 4) {
+      baris8[i] = 'Raja Hitam';
+      baris1[i] = 'Raja Putih';
+    }
+    baris7[i] = 'Prajurit Hitam';
+    baris2[i] = 'Prajurit Putih';
+  }
+  
+  for (let i = 0; i < 8; i++) {
+    if (i === 0) {
+      result.push(baris8)
+    } else if (i === 1) {
+      result.push(baris7)
+    } else if (i === 6) {
+      result.push(baris2)
+    } else if (i === 7) {
+      result.push(baris1)
+    } else {
+      result.push(baris3456)
+    }
+  }
+  return result;
 }
+
+console.log(makeChessboard());
 
 const printBoard = x => {
   // ... write your code here
+  for (let i = 0; i < x.length ; i++) {
+    console.log(x[i].join());
+  }
 }
 
 printBoard(makeChessboard())
--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,27 @@
 function tic_tac_toe_board () {
   // ...
+  let x = [];
+  let o = [];
+  let papan = [];
+  
+  for (let i = 0; i < 3; i++) {
+    let baris = [];
+    for (let j = 0; j < 3; j++) {
+      let acak = Math.random();
+      
+      if (acak < 0.5 && x < 5) {
+        x++;
+        baris.push('X');
+      } else if (o < 5) {
+        o++;
+        baris.push('O');
+      } else {
+        baris.push('X' || 'O');
+      }
+    }
+    papan.push(baris);
+  }
+  return papan;
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
