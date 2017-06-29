fab169dd59a3636d47e173113e5bf0245e1b9737
Ahmad Fajar <shin.fajar@gmail.com>
2017-06-29 18:25:45 +0700

    ganti pieces pakai unicode

--- chessboard.js
+++ chessboard.js
@@ -2,30 +2,34 @@
 
 const makeChessboard = () => {
   // --- board ---
-  let grid = () => {
+  let grid = (size => {
     let line = [];
-    for (var x = 0; x < 8; x++) {
-      line.push([])
-      for (var y = 0; y < 8; y++) {
-        line[x].push('[   ]')
+    for (var x = 0; x < size; x++) {
+      line.push([]);
+      for (var y = 0; y < size; y++) {
+        if((x + y) % 2 === 0) {
+          line[x].push('■');
+        } else {
+          line[x].push('□');
+        }
       }
     }
     return line;
-  }
+  });
 
-  let chessboard = grid();
+  let chessboard = grid(8)
 
   // --- pieces ---
-  const pieces = [   ['pawn'], ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook']  ];
-  const color = ['black', 'white'];
+  const blackPieces = [['♟'],['♜', '♞', '♝', '♚', '♛', '♝', '♞', '♜']];
+  const whitePieces = [['♙'],['♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖']];
 
   // --- placing ---
   for (var row = 0; row < 8; row++) {
     for (var col = 0; col < 8; col++) {
-      if (row === 0) chessboard[row][col] = `${color[0]} ${pieces[1][col]}`;
-      if (row === 1) chessboard[row][col] = `${color[0]} ${pieces[0]}`;
-      if (row === 6) chessboard[row][col] = `${color[1]} ${pieces[0]}`;
-      if (row === 7) chessboard[row][col] = `${color[1]} ${pieces[1][col]}`;
+      if (row === 0) chessboard[row][col] = `${blackPieces[1][col]}`;
+      if (row === 1) chessboard[row][col] = `${blackPieces[0]}`;
+      if (row === 6) chessboard[row][col] = `${whitePieces[0]}`;
+      if (row === 7) chessboard[row][col] = `${whitePieces[1][col]}`;
     }
   }
 
@@ -36,8 +40,7 @@ const makeChessboard = () => {
 
 const printBoard = x => {
   let display = [];
-  for (var i = 0; i <= x.length - 1; i++) display.push(x[i].join(' | '))
-  // console.log(display);
+  for (var i = 0; i <= x.length - 1; i++) display.push(x[i].join(' '))
 
   return console.log(display.join('\n'));
 }
@@ -48,3 +51,19 @@ module.exports = {
   makeChessboard,
   printBoard
 }
+
+// /*
+// white chess king	   ♔	U+2654
+// white chess queen	 ♕	U+2655
+// white chess rook	   ♖	U+2656
+// white chess bishop	 ♗	U+2657
+// white chess knight	 ♘	U+2658
+// white chess pawn	   ♙	U+2659
+//
+// black chess king	   ♚	U+265A
+// black chess queen	 ♛	U+265B
+// black chess rook	   ♜	U+265C
+// black chess bishop	 ♝	U+265D
+// black chess knight	 ♞	U+265E
+// black chess pawn	   ♟	U+265F
+// */
