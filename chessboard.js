843d6700bc0852aaf6fad7865711c986227b41e6
Adith <adithwidyapradipta@gmail.com>
2017-07-02 21:54:23 +0700

    nested arr 1 done

--- chessboard.js
+++ chessboard.js
@@ -1,14 +1,66 @@
 'use strict'
 
 const makeChessboard = () => {
-  let chessboard = []
+  let chessboard = [];
+  let tempChessboard = [];
 
   // ... write your code here
+  let kerajaan = ['Benteng', 'Kuda', 'Peluncur', 'Menteri', 'Raja', 'Peluncur', 'Kuda', 'Benteng'];
+  let troops = ['Pion'];
+  let warna  = ['Hitam', 'Putih'];
+  
+  //Symbol 
+  let kerajaanHitam = ['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜'];
+  let troopsHitam = ['♟'];
+  let kerajaanPutih = ['♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖'];
+  let troopsPutih = ['♙'];
+  
+  //looping kerajaan Hitam
+  for (let i=0; i<kerajaan.length; i++) {
+    // tempChessboard.push(`${kerajaan[i]} ${warna[0]}`);
+    tempChessboard.push(`${kerajaanHitam[i]}`);
+  }
+  
+  //looping troops Hitam
+  for (let i=0; i<kerajaan.length; i++) {
+    // tempChessboard.push(`${troops} ${warna[0]}`);
+    tempChessboard.push(`${troopsHitam}`);
+  }
+  
+  //looping kosongan
+  for (let i=0; i<32; i++) {
+    tempChessboard.push(`_`);
+  }
+  
+  //looping troops Putih
+  for (let i=0; i<kerajaan.length; i++) {
+    // tempChessboard.push(`${troops} ${warna[1]}`);
+    tempChessboard.push(`${troopsPutih}`);
+  }
+  
+  //looping kerajaan Putih
+  for (let i=0; i<kerajaan.length; i++) {
+    // tempChessboard.push(`${kerajaan[i]} ${warna[1]}`);
+    tempChessboard.push(`${kerajaanPutih[i]}`);
+  }
+  
+  let jarak = 8;
+ //looping final
+ for (let f = 0; f < tempChessboard.length; f += jarak) {
+   chessboard.push(tempChessboard.slice(f, f + jarak));
+ }
+  
   return chessboard
+  // return tempChessboard;
 }
+
+// console.log(makeChessboard());
 
 const printBoard = x => {
   // ... write your code here
+  for (var i = 0; i < x.length; i++) {
+    console.log(x[i].join(' ⎟ '));
+  }
 }
 
 printBoard(makeChessboard())
--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,38 @@
-function tic_tac_toe_board () {
+function tic_tac_toe_board() {
   // ...
+  var board = [];
+  var scoreX = '✖︎';
+  var scoreO = '◉';
+  var countX = 1;
+  var countO = 1;
+
+  for (let i = 0; i < 3; i++) {
+    board.push([]);
+    for (let j = 0; j < 3; j++) {
+      //Var Random
+      var random = Math.floor(Math.random() * 2);
+      //Condition if X or O more than or equal 5
+      if (countX >= 5) {
+        board[i].push(scoreO);
+      }
+      else if (countO >= 5) {
+        board[i].push(scoreX);
+      }
+      //Condition if random 0 or 1 then?
+      else {
+        if (random == 1) {
+          board[i].push(scoreX);
+          countX++;
+        }
+        else {
+          board[i].push(scoreO);
+          countO++;
+        }
+      }//else
+    }//for kedua
+  }//for pertama
+
+  return board;
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
