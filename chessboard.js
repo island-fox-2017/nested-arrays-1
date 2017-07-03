20c8a0b90f4ef134fe58c3e6d85961edccf4409a
gananggww <gananggww@gmail.com>
2017-07-03 09:07:27 +0700

    N-array 1

--- chessboard.js
+++ chessboard.js
@@ -1,18 +1,44 @@
 'use strict'
 
 const makeChessboard = () => {
-  let chessboard = []
+  let papanCatur= [];
+  let bidakRaja = ["Benteng", "Kuda", "Mentri", "Raja", "Ratu", "Mentri", "Kuda","Benteng"];
+  let bidakPion = ["Pion"];
 
-  // ... write your code here
-  return chessboard
+  for(var i=0 ; i<8; i++){
+    papanCatur.push([])
+    for (var j = 0; j < 8; j++) {
+      if (i == 0){
+        papanCatur[i].push(bidakRaja[j]+ " Putih")
+      }
+      else if (i == 1){
+        papanCatur[i].push(bidakPion[0] + " Putih")
+      }
+      else if (i == 6) {
+        papanCatur[i].push(bidakPion[0] + " Hitam")
+      }
+      else if (i == 7){
+        papanCatur[i].push(bidakRaja[j]+" Hitam")
+      }
+      else {
+        papanCatur[i].push("XXXXXXXXXX")
+      }
+
+
+    }
+  }
+
+var print = ""
+for (var i = 0; i <papanCatur.length; i++) {
+  print += papanCatur[i].join(" || ") + "\n"
+}
+  console.log(print);
 }
 
 const printBoard = x => {
-  // ... write your code here
+
 }
-
 printBoard(makeChessboard())
-
 module.exports = {
   makeChessboard,
   printBoard
--- tictactoe.js
+++ tictactoe.js
@@ -1,4 +1,43 @@
 function tic_tac_toe_board () {
+var hasil = [];
+var kali = 0
+var nol = 0
+// var hitung = [];
+for(var i= 0; i<3;i++){
+  let random = Math.floor(Math.random()*2)+0;
+  hasil.push([])
+  for(var j= 0 ; j<3 ; j++){
+    if(random == 1 ){
+      if(nol == 5 ){
+        hasil[i].push("o")
+        kali++
+      }
+      else if (nol < 5){
+        hasil[i].push("x")
+        nol++
+      }
+    }
+    if (random == 0){
+      if(kali == 5){
+        hasil[i].push("x")
+        nol++
+      }
+      else if(kali < 5){
+        hasil[i].push("o")
+        kali++
+      }
+      
+    }
+  }
+
+}
+var print = ""
+for(var i = 0; i<hasil.length; i++){
+  print += hasil[i].join(" | ")+ "\n";
+}
+return print;
+
+
   // ...
 }
 
