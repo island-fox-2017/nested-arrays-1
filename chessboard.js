8dbe5c1e79a3bf1cb493b389fa5ded849f530b3b
Renataclara <renataclarak@gmail.com>
2017-07-07 12:49:59 +0700

    Chessboard position and Tic Tac Toe board

--- chessboard.js
+++ chessboard.js
@@ -1,14 +1,35 @@
+
+// chess !!
 'use strict'
 
 const makeChessboard = () => {
-  let chessboard = []
+  let chessboard = [];
+  let players = 'Rook Knight Bishop Queen King Bishop Knight Rook'.split(' ');
+  let Pawns = 'Pawns'.repeat(8).match(/\D{5}/g); // repeat and split with count digit count
+  let Empty = '__________'.repeat(32).match(/\D{10}/g) // 32 empty positions
+  let BPlayers = players.concat(Pawns); // combine arrays
+  let WPlayers = Pawns.concat(players);
+  let black = [];
+  let white = [];
 
-  // ... write your code here
-  return chessboard
+  for (var z=0 ;z<BPlayers.length; z++) {
+    black[z] = 'Black '+BPlayers[z]
+    white[z] = 'White '+WPlayers[z]
+  }
+
+  var wholeBoard = black.concat(Empty).concat(white);
+
+  let i,j,seperator = 8 // after every x digit make into new line
+  for (i=0, j=wholeBoard.length; i<j; i+= seperator) {
+    chessboard.push(wholeBoard.slice(i, i+seperator))
+  }
+  return chessboard;
 }
 
-const printBoard = x => {
-  // ... write your code here
+const printBoard = x => { // from array in array into beautiful pretty line per line
+  for (var w = 0; w < x.length; w++) {
+        console.log(String(x[w].join(' || ')));
+   }
 }
 
 printBoard(makeChessboard())
--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,29 @@
+// A Random Tic Tac Toe Board 
+
 function tic_tac_toe_board () {
-  // ...
+  let board = [];
+  let totalX = 0;
+  let totalO = 0;
+  for (var i=0; i<3; i++) {
+    let baris = [];
+    for (var z=0; z<3; z++) {
+      var random = Math.floor(Math.random()*2);
+      if (random == 1 && totalO < 5 || totalX == 5) {
+        // selama totalO msh di bawa 5, tambahin skali lg biar totalny jd 5
+        baris.push('O');
+        totalO ++;
+      } else {
+        baris.push('X');
+        totalX ++;
+      }
+    }
+    board.push(baris);
+  }
+  return board.join('||');
 }
 
-console.log(tic_tac_toe_board()) // => make a random tic tic board
+console.log(tic_tac_toe_board())
+
+// console.log((Math.random())); // random number <1 && >0
+// console.log((Math.random()*2)); // random number <2 && >0
+// console.log(Math.floor(Math.random()*2)); // either 0 or 1, because we take the Math.floor of it
